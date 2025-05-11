3ed5b0522683ad9161878dc54c400b2c5332dbf9
Илья Коваленко <kovalenkoilyaden@gmail.com>
2025-05-11 14:31:08 +0300

    Add test integration

--- User_Service/src/test/java/com/IKov/User_Service/UserServiceApplicationTests.java
+++ User_Service/src/test/java/com/IKov/User_Service/UserServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.IKov.User_Service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testTest() {
+		Assertions.fail("Intentional failure in testTest()");
 	}
 
 }
