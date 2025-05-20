b859535ab2bb076d31b2b5ad78349370517f03ee
Илья Коваленко <kovalenkoilyaden@gmail.com>
2025-05-20 13:43:14 +0300

    Add logging in GrpcCLient

--- TimelineService/src/main/java/com/IKov/TimelineService/service/Impl/GrpcClientServiceImpl.java
+++ TimelineService/src/main/java/com/IKov/TimelineService/service/Impl/GrpcClientServiceImpl.java
@@ -5,6 +5,7 @@ import com.IKov.TimelineService.GetTwittsProto;
 import com.IKov.TimelineService.entity.TwittPost;
 import com.IKov.TimelineService.service.GrpcClientService;
 import com.IKov.TimelineService.web.mapper.TwittPostMapper;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class GrpcClientServiceImpl implements GrpcClientService {
 
     @GrpcClient("myGrpcService")
@@ -22,12 +24,26 @@ public class GrpcClientServiceImpl implements GrpcClientService {
 
     @Override
     public List<TwittPost> getRandomTwitts() {
+        log.info("Запрос случайных твитов: количество = {}", randomTwittsNumber);
 
         GetTwittsProto.GetTwittRequest request = GetTwittsProto.GetTwittRequest.newBuilder()
                 .setTwittsNumber(randomTwittsNumber)
                 .build();
-        GetTwittsProto.GetTwittReply reply = getTwittsBlockingStub.getTwitts(request);
+
+        log.debug("Сформирован gRPC-запрос: {}", request);
+
+        GetTwittsProto.GetTwittReply reply;
+        try {
+            reply = getTwittsBlockingStub.getTwitts(request);
+            log.info("Получен ответ от gRPC-сервиса: количество твитов = {}", reply.getTwitt(0).getTwittHeader());
+        } catch (Exception e) {
+            log.error("Ошибка при вызове gRPC-сервиса: {}", e.getMessage(), e);
+            throw e;
+        }
+
         List<TwittPost> twittPostList = TwittPostMapper.toDomainList(reply);
+        log.debug("Преобразование в доменные объекты завершено: {}", twittPostList);
+
         return twittPostList;
     }
 
